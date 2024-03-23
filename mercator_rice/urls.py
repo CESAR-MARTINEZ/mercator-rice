4e26418863b08353c8485c0bc8e9b899e7de35c4
CESAR-MARTINEZ <cesarfernando.11@gmail.com>
2024-03-22 21:31:46 -0500

    inlcude urls module store in main urls

--- mercator_rice/urls.py
+++ mercator_rice/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from store.views import showmenu
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', showmenu)
+    path('',include('store.urls'))
 ]
